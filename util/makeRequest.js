3f7362e314df78237792b425ae4c8e48df1c7af3
Zakariah Drummond <reqtified@gmail.com>
2018-05-22 21:24:29 +0100

    added a template string on concat

--- util/makeRequest.js
+++ util/makeRequest.js
@@ -1,20 +1,18 @@
 const axios = require('axios');
 
-
-function makeRequest(language){
-    return axios.get("https://api.github.com/search/repositories?q=language:" + language)
-        .then((res) => {
-            const repos = res.data.items;
-            let output = repos.map(item => {
-                 return {
-                        id: item.id,
-                        name: item.name,
-                        owner: item.owner.login,
-                        url: item.owner.url
-                    };
-            });
-            return output;
-        });
-    }
+function makeRequest(language) {
+  return axios.get(`https://api.github.com/search/repositories?q=language:${language}`).then(res => {
+    const repos = res.data.items;
+    let output = repos.map(item => {
+      return {
+        id: item.id,
+        name: item.name,
+        owner: item.owner.login,
+        url: item.owner.url
+      };
+    });
+    return output;
+  });
+}
 
 module.exports = makeRequest;
